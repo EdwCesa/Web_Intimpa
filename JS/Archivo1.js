e547281d332cc57afc1f6ebc0e0880904aa87dcb
EdwCesa <113655863+EdwCesa@users.noreply.github.com>
2022-10-09 13:36:33 -0500

    Inicio

---
+++ JS/Archivo1.js
@@ -0,0 +1,48 @@
+function palabreoinicio(){
+    let cadena=document.getElementById("pfinal").innerText;
+    cadena=cadena.substring(1)+cadena.charAt(0);
+    document.getElementById("pfinal").innerText=cadena;
+    setTimeout("palabreoinicio()",1000);
+ }
+
+function fecha(){
+    let fecha=new Date();
+    let dia=fecha.getDate();
+    let mes=fecha.getMonth()+1;
+    let anio=fecha.getFullYear();
+    let diasemana = '';
+
+switch (fecha.getDay()){
+    case 0: diasemana='Domingo'; break;
+    case 1: diasemana='Lunes'; break;
+    case 2: diasemana='Martes'; break;
+    case 3: diasemana='Miercoles'; break;
+    case 4: diasemana='Jueves'; break;
+    case 5: diasemana='Viernes'; break;
+    default: diasemana='Sabado'; break;
+}
+$("#cfecha").text(innerHTML=diasemana + "," + dia + "/" + mes + "/" + anio);
+}
+
+$(document).ready(function(){
+    fecha();
+    palabreoinicio();
+
+    $(".imgformulario").mouseenter(function() {
+    $(this).attr("style","transform:rotateY(360deg); transition: all 2s ease");
+    });
+        
+    $(".imgformulario").mouseleave(function() {
+    $(this).attr("style","transform:rotateY(-360deg); transition: all 2s ease");
+    });
+
+    $(".imgaside").mouseenter(function() {
+    $(this).attr("style","border: 3px solid gold");
+    });
+            
+    $(".imgaside").mouseleave(function() {
+    $(this).attr("style","border: 3px");
+    });    
+
+});
+
